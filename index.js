ef8b813663f7afc8cd654bd189e75caf33aa8bc8
gibranrafii <gibranrafi21@gmail.com>
2025-11-25 15:28:20 +0700

    mengaktifkan code yang dimatikan

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDatabase = require('./config/db');
-// const apiRoutes = require('./routes/api');
+const apiRoutes = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
@@ -8,7 +8,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use('/api/v1', apiRoutes);
+app.use('/api/v1', apiRoutes);
 
 async function startServer() {
     await connectDatabase();
